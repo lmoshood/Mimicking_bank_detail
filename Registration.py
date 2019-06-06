64c0bcdd7dba7f06d751197e03709e9d1ec91ee2
garantor <afolabisunday31@yahoo.com>
2019-06-06 04:38:51 +0100

    Creating a py code mimicking send money and checking your new balance.

--- Registration.py
+++ Registration.py
@@ -1,1 +1,41 @@
-print("hello world")
+todo = "send"
+check_balance = "balance"
+bal = 5000
+amount = 0
+new_bal = bal-amount
+
+
+def welcome():
+
+    print("Welcome Stranger")
+    username=input("what should i call you ? ")
+    print(" Alright " + username +" it is ")
+    print(username + " Your account Remaing is " + str(bal))
+    action=input(" What can i do for you today ? ")
+    if action == todo :
+        to_do()
+    elif action == check_balance:
+        check_bal()
+    else:
+        print(" Invalid Entry")
+
+
+def to_do():
+    Rname = input("What is the name of your Reciever ? ")
+    acct = input("Account Number if your receiver? ")
+    amount = input("Please enter amount to send? ")
+    if int(amount) >= bal:
+        print("Sorry, you are not allowed to empty your Account ")
+    else:
+        rnumber = input("Enter Reciever Phone number ")
+        Snumber = input("Enter your Phonr numbre just incase ")
+        print("If all the above details are correct, the reciever will be funded in Five minute, thanks ")
+        print("Your new balance is " + str(bal - int(amount)))
+        new_bal = bal - int(amount)
+        
+
+def check_bal():
+    print(new_bal)
+
+
+welcome()
