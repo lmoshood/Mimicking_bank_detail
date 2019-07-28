c79b9b63907b18f466edae333f8cd30742347479
garantor <afolabisunday31@yahoo.com>
2019-07-28 18:45:41 +0100

    New updated management, code. No Gui, create acoount with just username and password deposit, withdraw and check bal.

--- Registration.py
+++ Registration.py
@@ -1,45 +1,102 @@
-# Creating global variables to be used within the code#
-
-todo = "send"
-check_balance = "balance"
-bal = 5000
-amount = 0
-new_bal = bal-amount
-
-# function to welcome users and also ask what they will like to todo#
-
-def welcome():
-
-    print("Welcome Stranger")
-    username=input("what should i call you ? ")
-    print(" Alright " + username +" it is ")
-    print(username + " Your account Remaing is " + str(bal))
-    action=input(" What can i do for you today ? ")
-    if action == todo : # a conditional statemnet to help decide which action suite them the most
-        to_do()
-    elif action == check_balance:
-        check_bal()
-    else:
-        print(" Invalid Entry")
-
-#function to send money and details of the reciever
-
-def to_do():
-    Rname = input("What is the name of your Reciever ? ")
-    acct = input("Account Number if your receiver? ")
-    amount = input("Please enter amount to send? ")
-    if int(amount) >= bal:
-        print("Sorry, you are not allowed to empty your Account ")
-    else:
-        rnumber = input("Enter Reciever Phone number ")
-        Snumber = input("Enter your Phonr numbre just incase ")
-        print("If all the above details are correct, the reciever will be funded in Five minute, thanks ")
-        print("Your new balance is " + str(bal - int(amount)))
-        new_bal = bal - int(amount)
-        
-
-def check_bal():
-    print(new_bal)
+database = ['admin', 'admin']
+bal_database= []
+from random import randint
+import time
 
 
-welcome() #running the code
+class SavingsAcct():
+
+    def welcome(self, intialDeposit, amount_with):
+        print('Welcome to the New New Horizon Bank App')
+        ask = input('Do you have an account before?: Y(YES)/N(NO) or O to quit ').upper()
+        if ask == 'Y':
+            self.login(self, intialDeposit, amount_with)
+        elif ask == 'N':
+            self.reg(self,intialDeposit, amount_with)
+        elif ask == 'O':
+            print('Thanks for using this app, BYE')
+            quit()
+        else:
+            self.welcome(self, intialDeposit, amount_with)
+
+    def reg(self, intialDeposit, amount_with):
+        print('Create your account below')
+        reg_username = input('Enter your desire Username: ')
+        database.append(reg_username)
+        reg_pass1 = input('Enter password: ')
+        database.append(reg_pass1)
+        reg_pass2 = input('Confirm Password: ')
+        if reg_pass1 != reg_pass2:
+            print('Password dont Match')
+            return self.reg(SavingsAcct,intialDeposit, amount_with)
+        else:
+            account = randint(1111111111, 9999999999)
+            print('Your Account Has been succesfully Created ' + str(account))
+            return self.login(SavingsAcct, intialDeposit, amount_with)
+
+    def login(self, intialDeposit, amount_with):
+        Username= input('please Enter youur Username ')
+        if Username in database:
+            password = input('please Enter Your Password here: ')
+            if password in database:
+                print('Welcome ' + Username+ ' How can i help You Today?')
+                return self.action(SavingsAcct, Username, intialDeposit, amount_with)
+            else:
+                print('Password Note found, Enter Username Again')
+                return self.login(SavingsAcct, intialDeposit, amount_with)
+        else:
+            print('Incorrect Username, Try login Again')
+            return self.login(SavingsAcct, intialDeposit, amount_with)
+
+    def deposit(self, Username, amount_with):
+        intialDeposit =int(input('Enter amount You are saving into you account: '))
+        bal_database.append(intialDeposit)
+        print(str(intialDeposit) + ' Has been succesufully save to your account, Cheers')
+        return SavingsAcct.action(SavingsAcct, Username, intialDeposit, amount_with)
+
+    def withdraw(self, Username, intialDeposit):
+        print('Cheers '+ Username )
+        amount_with = int(input('Enter amount to Withdraw: '))
+        while amount_with > intialDeposit:
+            print('Insufficent Balance')
+            amount_with = int(input('Enter amount to Withdraw: '))
+        else:
+            with_code = randint(1111, 9999)
+            print('Thanks You, please see the Cashier for your cash and show them this Code ' +(str(with_code)))
+            return SavingsAcct.action(SavingsAcct, Username, intialDeposit, amount_with)
+
+    def check_bal(self, intialDeposit,amount_with ):
+        new_depo = intialDeposit - amount_with
+        if str(new_depo) < '0':
+            print('Your Balance is Empty')
+        else:
+            print('Your Balnace is ' + str(new_depo))
+            for i in range(1):
+                print('''Taking your back to the main menu in...\n...3''')
+                time.sleep(2)
+                print('...2')
+                time.sleep(2)
+                print('...1')
+                time.sleep(2)
+                return SavingsAcct.welcome(SavingsAcct,intialDeposit,amount_with)
+
+    def action(self, Username, intialDeposit, amount_with):
+        print('''please Choose from this Option below to start
+        Enter 1 to make deposit into your new account 
+        Enter 2 to Withdraw from the Account 
+        Enter 0 to Quite this program
+        Enter 3 to Check your Balance ''')
+        action = input('>>>>> ')
+        if action == '0':
+            SavingsAcct.welcome(self, intialDeposit, amount_with)
+        elif action == '1':
+            SavingsAcct.deposit(SavingsAcct, Username,amount_with)
+        elif action == '2':
+            self.withdraw(SavingsAcct,Username, intialDeposit)
+        elif action == '3':
+            SavingsAcct.check_bal(SavingsAcct,intialDeposit, amount_with)
+        else:
+            print('Invalid Command')
+            return SavingsAcct.action(SavingsAcct, Username, intialDeposit, amount_with)
+
+SavingsAcct.welcome(SavingsAcct,intialDeposit=0, amount_with=0)
